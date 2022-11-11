b64a467b5670f75b556deb07badf69062aae43af
Niraj Kendre <101392861+NirajKendre@users.noreply.github.com>
2022-11-11 20:13:47 +0530

    Add files via upload

---
+++ model/PaymentDeatilsDTO.java
@@ -0,0 +1,13 @@
+package com.example.model;
+
+
+
+public class PaymentDeatilsDTO {
+	
+	private Integer paymentId;
+	private Long netAmount;
+	private String paymentStatus;
+	private Integer ticketId;
+	private Integer customerId;
+
+}
--- model/PaymentDetails.java
+++ model/PaymentDetails.java
@@ -1,47 +1,33 @@
-package com.tripmaker.model;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+package com.example.model;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.data.repository.query.parser.Part.IgnoreCaseType;
 
+import antlr.collections.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PaymentDetails {
 	
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer paymentId;
+	private String paymentStatus;
+	private Integer customerId;
 	
-	@NotNull(message = "Payment Type is mandatory")
-	@NotEmpty(message = "Payment Type is mandatory")
-	@Size(min = 5, max = 50, message = "Description should be of 5-50 charecters")
-	@Pattern(regexp = "^[A-Z][a-z]*", message = "Only alphabets are allowed")
-	private String paymentType;
+	@OneToOne(cascade =  CascadeType.ALL)
+	private Package packages; 
 	
-	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd-MM-yyyy")
-	private LocalDate paymentDate;
-	
-	@OneToOne(cascade = CascadeType.ALL)
-	private Package packageDetails;
-	
-
 }
